c184c4fec4a2a54a92e4c73b4c5c94ec1e14a926
Nelson Yeung <nelsyeung@icloud.com>
2020-05-14 16:20:57 +0100

    Add file parser

---
+++ src/fileParser.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function getSudokuFromFile(filePath) {
+  return fs
+    .readFileSync(filePath)
+    .toString()
+    .split(/(?:\r\n|\n|\r)/)
+    .reduce((sudoku, line) => {
+      if (line.startsWith("Grid")) {
+        sudoku.push([]);
+      } else {
+        sudoku[sudoku.length - 1].push(
+          line.split("").map((x) => parseInt(x, 10))
+        );
+      }
+
+      return sudoku;
+    }, []);
+}
+
+module.exports = {
+  getSudokuFromFile,
+};
