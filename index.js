308c7d055e877ddd8af09594448d4f29a20f4842
Nelson Yeung <nelsyeung@icloud.com>
2020-05-15 22:29:01 +0100

    Add main execution script index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+/**
+ * Run this with `node index.js` to display all the sums or `node index.js -v`
+ * to display more information including the solution.
+ */
 const { getSudokuFromFile } = require("./src/fileParser.js");
 const { solve } = require("./src/solver.js");
 
-const allSudoku = getSudokuFromFile("./sudoku.txt");
-allSudoku.map((sudoku) => solve(sudoku));
+const allSudoku = getSudokuFromFile("sudoku.txt");
+const allSolutions = allSudoku.map((sudoku) => solve(sudoku));
+
+if (process.argv.length > 2 && process.argv[2] === "-v") {
+  allSolutions.forEach((solution, i) => {
+    const firstThree = solution[0].slice(0, 3);
+    const sum = firstThree.reduce((a, b) => a + b);
+    console.log(
+      `Grid ${String(++i).padStart(2, 0)}: ${firstThree.join(" + ")} = ${sum}`
+    );
+
+    solution.forEach((row) => console.log(row.join("")));
+    console.log("");
+  });
+} else {
+  allSolutions.forEach((solution) => {
+    console.log(solution[0].reduce((a, b, i) => (i < 3 ? a + b : a)));
+  });
+}
